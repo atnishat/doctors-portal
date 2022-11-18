ff94a619bbcf058eea96d3ec6afc1cc9104b0bc7
atnishat <nishat.mu47@gmail.com>
2022-11-18 20:52:38 +0600

    doctors appointment added

--- src/Routes/Routes/Route.js
+++ src/Routes/Routes/Route.js
@@ -1,4 +1,8 @@
+import Appoitment from "../../Pages/Appointment/Appointment/Appoitment";
+import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
 import Login from "../../Pages/Login/Login";
+import Signup from "../../Pages/Signup/Signup";
+import Privateroute from "../PrivateRoutes/Privateroute";
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../../Layout/Main");
@@ -20,9 +24,21 @@ const { default: Home } = require("../../Pages/Home/Home/Home");
         {
             path:'/login',
             element:<Login></Login>
+        },
+        {
+            path:'/signup',
+            element:<Signup></Signup>
+        },
+        {
+            path:'/appointment',
+            element:<Appoitment></Appoitment>
         }
     ]
-}
+        },
+        {
+            path:'/dashboard',
+            element:<Privateroute><Dashboard></Dashboard></Privateroute>
+        }
 
 ])
 
